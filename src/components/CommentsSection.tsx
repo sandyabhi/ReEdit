632bd96074565300dfc4dc5580d893140745e639
Sandeep Kumar Patel <abhisandy47@gmail.com>
2023-07-24 22:40:42 +0530

    display posts

---
+++ src/components/CommentsSection.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+interface CommentsSectionProps {}
+
+const CommentsSection: FC<CommentsSectionProps> = ({}) => {
+  return <div>CommentsSection</div>;
+};
+
+export default CommentsSection;
