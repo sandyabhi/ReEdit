c68c80c0a1f8a1f1ec103d3afbcb02b40486eb4b
Sandeep Kumar Patel <abhisandy47@gmail.com>
2023-07-25 22:59:38 +0530

    search

---
+++ src/app/settings/page.tsx
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: "Settings",
+  description: "Manage account and website settings",
+};
+
+import { FC } from "react";
+
+interface pageProps {}
+
+const page: FC<pageProps> = ({}) => {
+  return <div>page</div>;
+};
+
+export default page;
